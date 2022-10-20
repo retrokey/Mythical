e04ec6b589e771be6021d3e02d778c2971db4bad
Cosimo Celeste <gogliacosimoceleste@gmail.com>
2022-10-20 11:59:25 +0200

    Update permission.manager.ts

--- src/core/manager/permission.manager.ts
+++ src/core/manager/permission.manager.ts
@@ -11,7 +11,7 @@ export class PermissionManager {
         });
 
         if (response.status != 'success') {
-            return false;
+            return;
         }
 
         let session: UserSessionDefinition = Store.getState().session;
